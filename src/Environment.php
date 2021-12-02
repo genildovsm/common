ed9a15bd5d7b42b2a34d4415fa0dc8ffae948308
Genildo <genildovsm@gmail.com>
2021-12-02 13:54:55 -0300

    Update Environment.php

--- src/Environment.php
+++ src/Environment.php
@@ -5,19 +5,15 @@ namespace GenildoMartins\Common;
 class Environment 
 {
 	/**
-	 * Métod responsável por carregar as variáveis de ambiente
-	 *
-	 * @param string $dir "Caminho absoluto da pasta onde encontra-se o arquivo .env"
+	 * @param string $dir [Absolute path to file ".env"]
+	 * @return  void
 	 */
 	public static function load($dir)
 	{
-		// VERIFICA SE O ARQUIVO EXISTE
-		if (!file_exists($dir.'/.env'))
-		{
-			return false;
-		}
+		// Check if the file exists
+		if (!file_exists($dir.'/.env')) return false;
 
-		// DEFINE AS VARIÁVEIS DE AMBIENTE
+		// Set the enrironment variables
 		$lines = file($dir.'/.env');
 		foreach ($lines as $line){
 			putenv(trim($line));
